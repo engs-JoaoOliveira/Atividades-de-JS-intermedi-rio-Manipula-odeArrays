9b6f442bd34af534c79da0a9412d5af1fbe2f56b
engs-JoaoOliveira <engs-joaooliveira@camporeal.edu.br>
2026-05-08 22:02:56 -0300

    Adição
    Adição de botões de inserir ao inicio e ao final

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+const input = document.getElementById("input")
+const addFinal = document.getElementById("addFinal")
+const removFinal = document.getElementById("removFinal")
+const addInicio = document.getElementById("addInicio")
+const removInicio = document.getElementById("removInicio")
+
+
 let lista_nomes = ["Aléquis", "Ultron", "Kevinho"];
+let listaTela = document.getElementById('listaTela')
+
+function adicionarFinal(params) {
+}
 
 function mostrar_nomes(){
-    let listaTela = document.getElementById('listaTela')
 
     for(let nome of lista_nomes){
     let item = document.createElement('li')
@@ -11,5 +21,23 @@ function mostrar_nomes(){
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    mostrar_nomes()
+})
+
+addFinal.addEventListener("click", () => {
+    let valorInput = input.value
+
+    listaTela.innerHTML = ""
+
+    lista_nomes.push(valorInput)
+    mostrar_nomes()
+})
+
+addInicio.addEventListener("click", () => {
+    let valorInput = input.value
+
+    listaTela.innerHTML = ""
+
+    lista_nomes.unshift(valorInput)
     mostrar_nomes()
 })
