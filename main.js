afd6b56e74ea4939dd50466dc6b5dae878bfe2ca
engs-JoaoOliveira <engs-joaooliveira@camporeal.edu.br>
2026-05-09 14:49:35 -0300

    Criada as funções de filtrar e alterar para maiusculo

--- main.js
+++ main.js
@@ -3,22 +3,24 @@ const addFinal = document.getElementById("addFinal")
 const removFinal = document.getElementById("removFinal")
 const addInicio = document.getElementById("addInicio")
 const removInicio = document.getElementById("removInicio")
+
 const contar = document.getElementById("contar")
 
+const filtrar = document.getElementById("filtrar")
+const letraMaiuscula = document.getElementById("letraMaiuscula")
 
 let lista_nomes = ["Aléquis", "Ultron", "Kevinho"];
 let listaTela = document.getElementById('listaTela')
 
-function adicionarFinal(params) {
-}
+function mostrar_nomes(lista = lista_nomes){
+    listaTela.innerHTML = ""
 
-function mostrar_nomes(){
-
-    for(let nome of lista_nomes){
-    let item = document.createElement('li')
+    lista.forEach(nome => {
+        let item = document.createElement('li')
         item.innerText = nome
         listaTela.appendChild(item)
-    }
+    })
+
     contarNomes()
 }
 
@@ -42,13 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
 addFinal.addEventListener("click", () => {
     let valorInput = input.value
 
-    listaTela.innerHTML = ""
-
     lista_nomes.push(valorInput)
     mostrar_nomes()
 })
 removFinal.addEventListener("click", () => {
-    listaTela.innerHTML = ""
 
     lista_nomes.pop()
     mostrar_nomes()
@@ -56,14 +55,18 @@ removFinal.addEventListener("click", () => {
 addInicio.addEventListener("click", () => {
     let valorInput = input.value
 
-    listaTela.innerHTML = ""
-
     lista_nomes.unshift(valorInput)
     mostrar_nomes()
 })
 removInicio.addEventListener("click", () => {
-    listaTela.innerHTML = ""
-
     lista_nomes.shift()
     mostrar_nomes()
+})
+filtrar.addEventListener("click", () => {
+    const filtrados = lista_nomes.filter(nome => nome.length > 5)
+    mostrar_nomes(filtrados)
+})
+letraMaiuscula.addEventListener("click", () => {
+    const nomesMaiusculos = lista_nomes.map(nome => nome.toUpperCase())
+    mostrar_nomes(nomesMaiusculos)
 })
